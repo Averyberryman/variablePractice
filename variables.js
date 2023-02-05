c71444ed6c2682ca059ea1647eb12805098e480c
Avery Berryman <averyberryman@gmail.com>
2023-02-05 14:45:26 -0700

    added 3 Boolean variables

--- variables.js
+++ variables.js
@@ -1,3 +1,12 @@
+# string variables
+
 var email = "averyberryman@gmail.com"
 var name = "averyBerryman"
 var favorite color = "celticsGreen"
+
+# number variables
+
+var age = 29
+var height = 6
+var weight = 175
+
