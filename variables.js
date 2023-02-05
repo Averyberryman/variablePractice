2d9ad38b652818c51562b9ed8aa7dea0b83c6d5c
Avery Berryman <averyberryman@gmail.com>
2023-02-05 15:04:41 -0700

    add console log correction

--- variables.js
+++ variables.js
@@ -1,16 +1,13 @@
-# string variables
 
 var email = "averyberryman@gmail.com"
 var mynameis = "averyBerryman"
 var favoriteColor = "celticsGreen"
 
-# number variables
 
 var age = 29
 var height = 6
 var weight = 175
 
-# Boolean variables
 
 var isSleepy = false
 var isReadyForGym = true
